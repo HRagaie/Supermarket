7ac608b852685a8f4fc882a779f088c2fe0cecfe
HRagaie <hodam.ragaie@gmail.com>
2019-04-19 13:51:08 +0200

    Update view.js

--- view.js
+++ view.js
@@ -17,7 +17,6 @@ public class GameView extends JFrame {
 
 	public GameView() {
 		setSize(1000, 500);
-		setTitle("Alien Invasion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension dim = tk.getScreenSize();
@@ -27,9 +26,7 @@ public class GameView extends JFrame {
 
 		JPanel title = new JPanel();
 		JLabel label1 = new JLabel("Invaded City");
-		cycle = new JButton("Next Cycle");
 		title.add(label1);
-		title.add(cycle);
 		add(title, BorderLayout.NORTH);
 		
 			
@@ -38,20 +35,16 @@ public class GameView extends JFrame {
 		for (int i = 0; i < 100; i++) {
 			JButton mapButton = new JButton();
 			addButton(mapButton);
-	}
+		}
 		add(buttonPanel, BorderLayout.CENTER);	
 
 		JPanel textPanel = new JPanel();
 		textPanel.setLayout(new GridLayout(3, 1, 4, 4));
 		
 		
-		availableUnits = new JTextArea();
+		availableUnits = new JTextArea(2,2);
 		//availableUnits.setPreferredSize(new Dimension(200, 200));
-		availableUnits.setEditable(false);
-		availableUnits.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-		availableUnits.setText("availableUnits\n ");
-		availableUnits.setLineWrap(true);
-		availableUnits.setWrapStyleWord(true); // words not broken up
+	
 		
 		unitButtonPanel = new JPanel();
 		unitButtonPanel.setLayout(new GridLayout(2, 2, 2, 2));
@@ -68,20 +61,10 @@ public class GameView extends JFrame {
 
 		JTextArea respondingUnits = new JTextArea(4,4);
 		//respondingUnits.setPreferredSize(new Dimension(200, 200));
-		respondingUnits.setEditable(false);
-		respondingUnits.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-		respondingUnits.setText("respondingUnits\n ");
-		respondingUnits.setLineWrap(true);
-		respondingUnits.setWrapStyleWord(true);
 		textPanel.add(respondingUnits);
 
 		JTextArea treatingUnits = new JTextArea(4,4);
 		//treatingUnits.setPreferredSize(new Dimension(200, 200));
-		treatingUnits.setEditable(false);
-		treatingUnits.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-		treatingUnits.setText("treatingUnits\n ");
-		treatingUnits.setLineWrap(true);
-		treatingUnits.setWrapStyleWord(true);
 		textPanel.add(treatingUnits);
 
 		add(textPanel, BorderLayout.EAST);
@@ -92,11 +75,6 @@ public class GameView extends JFrame {
 		JScrollPane scrollbar1 = new JScrollPane(informationDisplay, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollbar1.setSize( 20, 20 );
 		informationDisplay.setPreferredSize(new Dimension(getWidth(), 200));
-		informationDisplay.setEditable(false);
-		informationDisplay.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-		informationDisplay.setText("informationDisplay\n ");
-		informationDisplay.setLineWrap(true);
-		informationDisplay.setWrapStyleWord(true);
 
 		infoPanel.add(scrollbar1);
 		add(infoPanel, BorderLayout.SOUTH);
@@ -106,11 +84,6 @@ public class GameView extends JFrame {
 		disasterPanel.setLayout(new BorderLayout());
 		disasterDisplay = new JTextArea();
 		disasterDisplay.setPreferredSize(new Dimension(getWidth(), 200));
-		disasterDisplay.setEditable(false);
-		disasterDisplay.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-		disasterDisplay.setText("disasterDisplay\n ");
-		disasterDisplay.setLineWrap(true);
-		disasterDisplay.setWrapStyleWord(true);
 
 		disasterPanel.add(disasterDisplay);
 		add(disasterPanel, BorderLayout.WEST);
